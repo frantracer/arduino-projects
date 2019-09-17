340396402fff6e17a4cb66130f3247cb87004d94
frantracer <frantracer@gmail.com>
2019-09-17 18:49:42 +0200

    Refactor some variables naming

--- ascensor/ascensor.ino
+++ ascensor/ascensor.ino
@@ -1,28 +1,28 @@
 
-#define encoderPin1 2
-#define encoderPin2 3
+#define PIN_1_ENCODER         2
+#define PIN_2_ENCODER         3
 
 // pulsadores llamada
-#define llamada1 4         //Pulsador llamada planta sotano
-const int  llamada2 = 5;         //pulsador llamada planta baja
-const int  llamada3 = 6;         //puladador llamada planta primera
+#define PIN_PULSADOR_1        4    // Pulsador llamada planta sotano
+#define PIN_PULSADOR_2        5    // Pulsador llamada planta baja
+#define PIN_PULSADOR_3        6    // Pulsador llamada planta primera
 
 // sensores de planta
-const int final1 = 7;            //Final carrera plata sotano
-const int final2 = 8;            //final carrera planta baja
-const int final3 = 9;            //final carrera planta primera
+#define PIN_FINAL_CARRERA_1   7    // Final carrera planta sotano
+#define PIN_FINAL_CARRERA_2   8    // Final carrera planta baja
+#define PIN_FINAL_CARRERA_3   9    // Final carrera planta primera
 
 // led indicadores de plantas
-const int led1 = 39;             //led indicador de planta sotano
-const int led2 = 41;             //led indicador de planta baja
-const int led3 = 43;             //led indicador de planta primera
+#define PIN_LED_PLANTA_1      39   // Led indicador de planta sotano
+#define PIN_LED_PLANTA_2      41   // Led indicador de planta baja
+#define PIN_LED_PLANTA_3      43   // Led indicador de planta primera
 
 // movimiento motor
-const int bajar = 23;            //salida rele bajada motor
-const int subir = 25;            //salida rele subida motor
-const int parolento = 27;        //salida rele paro lento
+#define PIN_MOTOR_BAJADA      23   // Salida rele bajada motor
+#define PIN_MOTOR_SUBIDA      25   // Salida rele subida motor
+#define PIN_MOTOR_PARO_LENTO  27   // Salida rele paro lento
 
-// variable para estado anterior del encoder
+// variables de estado del encoder
 int valor_encoder = 0;
 int estado_encoder_ultimo = 0b00;
 
@@ -42,25 +42,42 @@ bool vllamada3, vllamada3Ant;
 void setup() {
   Serial.begin (9600);
   
-  //PIN DE LLAMADAS COMO ENTRADAS
-  pinMode(llamada1, INPUT);
-  pinMode(llamada2, INPUT);
-  pinMode(llamada3, INPUT);
+  // PIN DE LLAMADAS COMO ENTRADAS
+  pinMode(PIN_PULSADOR_1, INPUT);
+  pinMode(PIN_PULSADOR_2, INPUT);
+  pinMode(PIN_PULSADOR_3, INPUT);
 
-  //PIN DE FINAL DE CARRERA COMO ENTRADAS
-  pinMode(final1, INPUT);
-  pinMode(final2, INPUT);
-  pinMode(final3, INPUT);
+  // PIN DE FINAL DE CARRERA COMO ENTRADAS
+  pinMode(PIN_FINAL_CARRERA_1, INPUT);
+  pinMode(PIN_FINAL_CARRERA_2, INPUT);
+  pinMode(PIN_FINAL_CARRERA_3, INPUT);
 
-  //PINES DE LED INDICADORES DE PLANTAS
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+  // PINES DE LED INDICADORES DE PLANTAS
+  pinMode(PIN_LED_PLANTA_1, OUTPUT);
+  pinMode(PIN_LED_PLANTA_2, OUTPUT);
+  pinMode(PIN_LED_PLANTA_3, OUTPUT);
 
-  //PINES DE MOVIMIENTO DE MOTOR SALIDAS
-  pinMode(subir, OUTPUT);
-  pinMode(bajar, OUTPUT);
-  pinMode(parolento, OUTPUT);
+  // PINES DE MOVIMIENTO DE MOTOR SALIDAS
+  pinMode(PIN_MOTOR_BAJADA, OUTPUT);
+  pinMode(PIN_MOTOR_SUBIDA, OUTPUT);
+  pinMode(PIN_MOTOR_PARO_LENTO, OUTPUT);
+
+  // INICIALIZAR TODOS LOS COMPONENTES DEL SISTEMA
+
+  // Para motor al iniciar
+  digitalWrite(PIN_MOTOR_BAJADA, HIGH);
+  digitalWrite(PIN_MOTOR_SUBIDA, HIGH);
+  digitalWrite(PIN_MOTOR_PARO_LENTO, HIGH);
+
+  // Activar pines del encoder
+  pinMode(PIN_1_ENCODER, INPUT_PULLUP);
+  pinMode(PIN_2_ENCODER, INPUT_PULLUP);
+  
+  digitalWrite(PIN_1_ENCODER, HIGH);
+  digitalWrite(PIN_2_ENCODER, HIGH);
+
+  attachInterrupt(0, actualizar_encoder, CHANGE);
+  attachInterrupt(1, actualizar_encoder, CHANGE);
 
   // Detectar piso actual
   piso_actual = detectar_piso_actual();
@@ -68,57 +85,42 @@ void setup() {
   // Estado inicial pines de entrada y salida
   switch(piso_actual) {
     case 1:
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
+      digitalWrite(PIN_LED_PLANTA_1, HIGH);
+      digitalWrite(PIN_LED_PLANTA_2, LOW);
+      digitalWrite(PIN_LED_PLANTA_3, LOW);
       break;
     case 2:
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, LOW);
+      digitalWrite(PIN_LED_PLANTA_1, LOW);
+      digitalWrite(PIN_LED_PLANTA_2, HIGH);
+      digitalWrite(PIN_LED_PLANTA_3, LOW);
       break;
     case 3:
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, HIGH);
+      digitalWrite(PIN_LED_PLANTA_1, LOW);
+      digitalWrite(PIN_LED_PLANTA_2, LOW);
+      digitalWrite(PIN_LED_PLANTA_3, HIGH);
       break;
     case -1:
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, HIGH);
+      digitalWrite(PIN_LED_PLANTA_1, HIGH);
+      digitalWrite(PIN_LED_PLANTA_2, HIGH);
+      digitalWrite(PIN_LED_PLANTA_3, HIGH);
       break;
   }
-
-  digitalWrite(subir, HIGH);
-  digitalWrite(bajar, HIGH);
-  
-  digitalWrite(parolento, HIGH);
-
-  // Activar pines del encoder
-  pinMode(encoderPin1, INPUT_PULLUP);
-  pinMode(encoderPin2, INPUT_PULLUP);
-  
-  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
-  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
-
-  attachInterrupt(0, actualizar_encoder, CHANGE);
-  attachInterrupt(1, actualizar_encoder, CHANGE);
 }
 
 void loop() {
 
   if(piso_destino == -1) {
-    vllamada1 = digitalRead(llamada1);
+    vllamada1 = digitalRead(PIN_PULSADOR_1);
     if (vllamada1 && vllamada1Ant)                     // asigadores de valor numerico para cada final de llamada
         piso_destino = 1;                               // cambio el nombre a pisodestino
     vllamada1Ant = vllamada1;   
                                    
-    vllamada2 = digitalRead(llamada2);
+    vllamada2 = digitalRead(PIN_PULSADOR_2);
     if (vllamada2 && vllamada2Ant)
         piso_destino = 2;
     vllamada2Ant = vllamada2;
   
-    vllamada3 = digitalRead(llamada3);
+    vllamada3 = digitalRead(PIN_PULSADOR_3);
     if (vllamada3 && vllamada3Ant)
         piso_destino = 3;
     vllamada3Ant = vllamada3;
@@ -127,18 +129,18 @@ void loop() {
   switch (piso_destino) {
       case 1:                                   //caso para la llamada desde el sotano
           if (piso_actual > 1)                   //si el piso en el que esta el montacargas es mayor a 1
-            digitalWrite (bajar, LOW);         // esto hara bajar al motor
-            digitalWrite (led1, HIGH);          //al pulsar el pulsador del sotano el led 1 del sotano se pone intermitente
+            digitalWrite (PIN_MOTOR_BAJADA, LOW);         // esto hara bajar al motor
+            digitalWrite (PIN_LED_PLANTA_1, HIGH);          //al pulsar el pulsador del sotano el led 1 del sotano se pone intermitente
             //delay(500);                         
-            digitalWrite (led1, LOW);
+            digitalWrite (PIN_LED_PLANTA_1, LOW);
             //delay(500);
             Serial.println(valor_encoder);
-          if (digitalRead(final1)) {          //cuando el final de carrera de la planta sotano se active significa que ya esta el montacargas en la planta sotano y detendra el motor
-            digitalWrite (subir, HIGH);
-            digitalWrite (bajar, HIGH);
-            digitalWrite (led1, HIGH);        //el led 1 se pone fijo cuando se activa el final de carrera del sotano 
-            digitalWrite (led2, LOW);         // y se apaga el led de la planta que estaba
-            digitalWrite (led3, LOW);
+          if (digitalRead(PIN_FINAL_CARRERA_1)) {          //cuando el final de carrera de la planta sotano se active significa que ya esta el montacargas en la planta sotano y detendra el motor
+            digitalWrite (PIN_MOTOR_SUBIDA, HIGH);
+            digitalWrite (PIN_MOTOR_BAJADA, HIGH);
+            digitalWrite (PIN_LED_PLANTA_1, HIGH);        //el led 1 se pone fijo cuando se activa el final de carrera del sotano 
+            digitalWrite (PIN_LED_PLANTA_2, LOW);         // y se apaga el led de la planta que estaba
+            digitalWrite (PIN_LED_PLANTA_3, LOW);
             piso_actual = 1;
             piso_destino = -1;
             Serial.println(valor_encoder);
@@ -147,20 +149,20 @@ void loop() {
 
     case 2:                                     //caso para la llamada desde la planta baja
           if (piso_actual > 2)                   //si el piso en el que esta el montacargas es mayor a 2
-            digitalWrite(bajar, LOW);          //esto hara bajar al motor
+            digitalWrite(PIN_MOTOR_BAJADA, LOW);          //esto hara bajar al motor
           else if (piso_actual < 2)              //si el piso en el que esta el montagargas es menor a 2
-            digitalWrite(subir, LOW);          //esto hara subir al motor
-            digitalWrite (led2, HIGH);          //al pulsar el pulsador del baja el led 2 de la planta baja se pone intermitente
+            digitalWrite(PIN_MOTOR_SUBIDA, LOW);          //esto hara subir al motor
+            digitalWrite (PIN_LED_PLANTA_2, HIGH);          //al pulsar el pulsador del baja el led 2 de la planta baja se pone intermitente
             //delay(500);
-            digitalWrite (led2, LOW);
+            digitalWrite (PIN_LED_PLANTA_2, LOW);
             //delay(500);
             Serial.println(valor_encoder);
-          if (digitalRead(final2)) {          //cuando el final de carrera de la planta baja se active significa que ya esta el montacargas en la planta baja y detendra motor
-            digitalWrite(subir, HIGH);
-            digitalWrite (bajar, HIGH);
-            digitalWrite (led2, HIGH);        //el led 2 se pone fijo cuando se activa el final de carrera de la planta baja 
-            digitalWrite (led1, LOW);         // y se apaga el led de la planta que estaba
-            digitalWrite (led3, LOW);
+          if (digitalRead(PIN_FINAL_CARRERA_2)) {          //cuando el final de carrera de la planta baja se active significa que ya esta el montacargas en la planta baja y detendra motor
+            digitalWrite(PIN_MOTOR_SUBIDA, HIGH);
+            digitalWrite (PIN_MOTOR_BAJADA, HIGH);
+            digitalWrite (PIN_LED_PLANTA_2, HIGH);        //el led 2 se pone fijo cuando se activa el final de carrera de la planta baja 
+            digitalWrite (PIN_LED_PLANTA_1, LOW);         // y se apaga el led de la planta que estaba
+            digitalWrite (PIN_LED_PLANTA_3, LOW);
             piso_actual = 2;
             piso_destino = -1;
             Serial.println(valor_encoder);
@@ -169,18 +171,18 @@ void loop() {
        
     case 3:                                //caso para la llamada desde la planta primera
           if (piso_actual < 3)              //si el piso en el que esta el montacargas es menor a 3
-            digitalWrite(subir, LOW);     //esto hara subir al motor
-            digitalWrite (led3, HIGH);     //al pulsar el pulsador de la plata primera el led 3 de la planta primera se pone intermitente
+            digitalWrite(PIN_MOTOR_SUBIDA, LOW);     //esto hara subir al motor
+            digitalWrite (PIN_LED_PLANTA_3, HIGH);     //al pulsar el pulsador de la plata primera el led 3 de la planta primera se pone intermitente
             //delay(500);
-            digitalWrite (led3, LOW);
+            digitalWrite (PIN_LED_PLANTA_3, LOW);
             //delay(500);
             Serial.println(valor_encoder);
-          if (digitalRead(final3)) {      //cuando el final de carrera de la planta primera se active significa que ya esta el montacargas en la planta primera y detendra el motor
-            digitalWrite(subir, HIGH);
-            digitalWrite (bajar, HIGH);
-            digitalWrite (led3, HIGH);    //el led 3 se pone fijo cuando se activa el final de carrera de la planta primera 
-            digitalWrite (led1, LOW);     // y se apaga el led de la planta que estaba
-            digitalWrite (led2, LOW);    
+          if (digitalRead(PIN_FINAL_CARRERA_3)) {      //cuando el final de carrera de la planta primera se active significa que ya esta el montacargas en la planta primera y detendra el motor
+            digitalWrite(PIN_MOTOR_SUBIDA, HIGH);
+            digitalWrite (PIN_MOTOR_BAJADA, HIGH);
+            digitalWrite (PIN_LED_PLANTA_3, HIGH);    //el led 3 se pone fijo cuando se activa el final de carrera de la planta primera 
+            digitalWrite (PIN_LED_PLANTA_1, LOW);     // y se apaga el led de la planta que estaba
+            digitalWrite (PIN_LED_PLANTA_2, LOW);    
             piso_actual = 3;
             piso_destino = -1;
             Serial.println(valor_encoder);
@@ -193,11 +195,11 @@ void loop() {
 int detectar_piso_actual() {
   int piso = -1;
   
-  if(digitalRead(final1))
+  if(digitalRead(PIN_FINAL_CARRERA_1))
     piso = 1;
-  else if(digitalRead(final2))
+  else if(digitalRead(PIN_FINAL_CARRERA_2))
     piso = 2;
-  else if(digitalRead(final3))
+  else if(digitalRead(PIN_FINAL_CARRERA_3))
     piso = 3;
   else
     piso = -1;
@@ -206,8 +208,8 @@ int detectar_piso_actual() {
 }
 
 void actualizar_encoder() {
-  int MSB = digitalRead(encoderPin1);  //MSB = Bit más significativo
-  int LSB = digitalRead(encoderPin2);  //LSB = Bit menos significativo
+  int MSB = digitalRead(PIN_1_ENCODER);  //MSB = Bit más significativo
+  int LSB = digitalRead(PIN_2_ENCODER);  //LSB = Bit menos significativo
 
   int estado_encoder_actual = (MSB << 1) | LSB;  // Convertir valor pines en un estado
   int sum = (estado_encoder_ultimo << 2) | estado_encoder_actual;  // Juntar estado anterior con el actual
