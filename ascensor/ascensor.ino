ce639c1ed253fe476f4e5b5d2d168f76ac0c9310
frantracer <frantracer@gmail.com>
2019-09-18 18:30:16 +0200

    Ascensor: Cambiar nombre plataforma por montacargas

--- ascensor/ascensor.ino
+++ ascensor/ascensor.ino
@@ -142,32 +142,32 @@ void loop() {
     switch (piso_destino) {
       case 1:                                   //caso para la llamada desde el sotano
         if (piso_actual > 1) {                 //si el piso en el que esta el montacargas es mayor a 1
-          bajar_plataforma();
+          bajar_montacargas();
         }
         if (digitalRead(PIN_FINAL_CARRERA_1)) {  //cuando el final de carrera de la planta sotano se active significa que ya esta el montacargas en la planta sotano y detendra el motor
-          parar_plataforma();
+          parar_montacargas();
           piso_actual = 1;
           piso_destino = -1;
         }         
         break;
       case 2:                                     //caso para la llamada desde la planta baja
         if (piso_actual > 2) {                  //si el piso en el que esta el montacargas es mayor a 2
-          bajar_plataforma();
+          bajar_montacargas();
         } else if (piso_actual < 2) {              //si el piso en el que esta el montagargas es menor a 2
-          subir_plataforma();
+          subir_montacargas();
         }
         if (digitalRead(PIN_FINAL_CARRERA_2)) {          //cuando el final de carrera de la planta baja se active significa que ya esta el montacargas en la planta baja y detendra motor
-          parar_plataforma();
+          parar_montacargas();
           piso_actual = 2;
           piso_destino = -1;
         }
         break;
       case 3:                                //caso para la llamada desde la planta primera
         if (piso_actual < 3) {              //si el piso en el que esta el montacargas es menor a 3
-          subir_plataforma();
+          subir_montacargas();
         }
         if (digitalRead(PIN_FINAL_CARRERA_3)) {      //cuando el final de carrera de la planta primera se active significa que ya esta el montacargas en la planta primera y detendra el motor
-          parar_plataforma();
+          parar_montacargas();
           piso_actual = 3;
           piso_destino = -1;
         }
@@ -179,17 +179,17 @@ void loop() {
   encender_leds(piso_actual, piso_destino);
 }
 
-void parar_plataforma(){
+void parar_montacargas(){
   digitalWrite(PIN_MOTOR_SUBIDA, HIGH);
   digitalWrite(PIN_MOTOR_BAJADA, HIGH);
 }
 
-void subir_plataforma(){
+void subir_montacargas(){
   digitalWrite(PIN_MOTOR_SUBIDA, LOW);
   digitalWrite(PIN_MOTOR_BAJADA, HIGH);
 }
 
-void bajar_plataforma(){
+void bajar_montacargas(){
   digitalWrite(PIN_MOTOR_SUBIDA, HIGH);
   digitalWrite(PIN_MOTOR_BAJADA, LOW);
 }
